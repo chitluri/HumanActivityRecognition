b8252518e648acfb0a48bb17f2d23c6b1bf61798
chitluri <gopi_chitluri@yahoo.com>
2015-04-24 18:20:15 -0400

    Fixing scripts for Freq and Time files

--- WorkingFolder/genTestData3.sh
+++ WorkingFolder/genTestData3.sh
@@ -4,7 +4,7 @@
 cat test/X_test.txt | awk '{out=$1; for(i=2;i<=265;i++){out=out","$i};for(j=555;j<=NF;j++){out=out","$j}; print out}' > test/X_test_clean.txt
 
 #Create header file from features.txt
-printf "@relation HumanActivityRecognition\n\n@attribute Subject {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'}\n" > test/header.arff &&  cat features.txt | tr ',' '-' | awk '{ print "@attribute "$1$2" numeric" }' >> test/header.arff && printf "@attribute class {'1','2','3','4','5','6'}\n\n@data\n" >> test/header.arff
+printf "@relation HumanActivityRecognition\n\n@attribute Subject {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'}\n" > test/header.arff &&  cat features.txt | tr ',' '-' | awk '{ print "@attribute "$1$2" numeric" }' | sed '266,554d' >> test/header.arff && printf "@attribute class {'1','2','3','4','5','6'}\n\n@data\n" >> test/header.arff
 
 #Create data.csv from subject_test.txt X_test_clean.txt y_test.txt
 paste -d':' test/subject_test.txt test/X_test_clean.txt test/y_test.txt | tr '\r' '&' | tr ':' ',' |  sed 's/&//' > test/data.csv
--- WorkingFolder/genTrainData3.sh
+++ WorkingFolder/genTrainData3.sh
@@ -4,7 +4,7 @@
 cat train/X_train.txt | awk '{out=$1; for(i=2;i<=265;i++){out=out","$i};for(j=555;j<=NF;j++){out=out","$j}; print out}' > train/X_train_clean.txt
 
 #Create header file from features.txt
-printf "@relation HumanActivityRecognition\n\n@attribute Subject {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'}\n" > train/header.arff &&  cat features.txt | tr ',' '-' | awk '{ print "@attribute "$1$2" numeric" }' >> train/header.arff && printf "@attribute class {'1','2','3','4','5','6'}\n\n@data\n" >> train/header.arff
+printf "@relation HumanActivityRecognition\n\n@attribute Subject {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'}\n" > train/header.arff &&  cat features.txt | tr ',' '-' | awk '{ print "@attribute "$1$2" numeric" }' | sed '266,554d' >> train/header.arff && printf "@attribute class {'1','2','3','4','5','6'}\n\n@data\n" >> train/header.arff
 
 #Create data.csv from subject_train.txt X_train_clean.txt y_train.txt
 paste -d':' train/subject_train.txt train/X_train_clean.txt train/y_train.txt | tr '\r' '&' | tr ':' ',' |  sed 's/&//' > train/data.csv
